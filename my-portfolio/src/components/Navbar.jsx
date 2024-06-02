4af747237c71de932519998815dee8c87cf47c88
Davinderpal Singh <78408207+DavinderSingh810@users.noreply.github.com>
2024-06-03 00:25:34 +0530

    update

---
+++ my-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "../Styles/Nav.css";
+
+function Navbar() {
+  return (
+    <div className='navd'>
+      
+        <nav className='navbar'>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+          </ul>
+        </nav>
+      
+    </div>
+  );
+}
+
+export default Navbar;
