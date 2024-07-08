f39cd6b2eee039d199f5aeaf4805291d06bdd5b7
Davinderpal Singh <78408207+DavinderSingh810@users.noreply.github.com>
2024-07-08 20:21:21 +0530

    update

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import "../Styles/Nav.css";
+import imageSrc from "../img/Image.png"; // Import the image
 
 function Navbar() {
   return (
     <div className='navd'>
-      
-        <nav className='navbar'>
-            <div>pfpandname</div>
-            <div className='list'>
+      <nav className='navbar'>
+        <div className='pfp'>
+          
+          <img src={imageSrc} alt="" />
+           <h3>Davinderpal Singh</h3>
+        </div>
+        <div className='list'>
           <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Projects</a></li>
           </ul>
-          </div>
-        </nav>
-      
+        </div>
+      </nav>
     </div>
   );
 }
