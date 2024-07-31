8c7de5e5a69c726d3266eca99c269cf2d4d5cfa5
Davinderpal Singh <78408207+DavinderSingh810@users.noreply.github.com>
2024-07-31 18:01:05 +0530

    update

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -4,8 +4,17 @@ import "../Styles/project.css";
 function Projects() {
   return (
     <div>
+    <div>
       <h2 className='prohead'>Projects</h2>
+
       </div>
+
+    <div>
+      
+    </div>
+
+      </div>
+
     
   )
 }
